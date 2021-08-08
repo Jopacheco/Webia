dbf83304a7e8e00ca8c59651fea7e55efab78305
Jorge Pacheco <37810263+Jopacheco@users.noreply.github.com>
2021-08-08 22:21:11 +0000

    Add files via upload

---
+++ js/chunk-2d237c3d.5a501b5e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d237c3d"],{fd09:function(o){o.exports=JSON.parse('[{"_id":{"$oid":"60fc7c8096581c6110c5b158"},"active":true,"designacao":"RPS ","area":2,"descricao":"Jogo Rocha Papel Tesoura","nivel":4,"uriimagem":"./assets/imagem.png","urlvideo":"http//www.abc.pt","urlficheiro2":"http://www.video.pt","urlficheiro1":"http://www.urlapontador.pt","urlapontador1":"http://www.urlapontador.pt","urlapontador2":"http://www.urlapontador.pt","id":1}]')}}]);
+//# sourceMappingURL=chunk-2d237c3d.5a501b5e.js.map
